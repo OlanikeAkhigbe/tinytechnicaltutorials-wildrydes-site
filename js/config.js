2dc3d2f01f63a3df62aded6dc6af3aaac3ad1e53
OlanikeAkhigbe <132076512+OlanikeAkhigbe@users.noreply.github.com>
2025-08-17 16:36:18 +0100

    Update config.js
    commented clientSecret

--- js/config.js
+++ js/config.js
@@ -2,13 +2,14 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_74zwpBmlD', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '17almcj18d2o447h6qi5cmfrnk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientSecret: '12qnloqfh20n84t68kghu3ff34omn96rtruke9cm8ps56tjld70l',
+        //userPoolClientSecret: '12qnloqfh20n84t68kghu3ff34omn96rtruke9cm8ps56tjld70l',
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
